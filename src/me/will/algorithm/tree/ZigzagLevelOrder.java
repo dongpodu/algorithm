2ff01b17ff842eb2d828393470a0a56d57c306ac
willdu <695465951@qq.com>
2024-02-25 14:59:27 +0800

    ZigzagLevelOrder

--- src/me/will/algorithm/tree/ZigzagLevelOrder.java
+++ src/me/will/algorithm/tree/ZigzagLevelOrder.java
@@ -1,7 +1,6 @@
 package me.will.algorithm.tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 给你二叉树的根节点 root ，返回其节点值的锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）
@@ -10,39 +9,37 @@ public class ZigzagLevelOrder {
 
 	public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 		List<List<Integer>> result = new ArrayList<>();
-		traverse(root, true, result);
-		return result;
-	}
 
-	public static void traverse(TreeNode root, boolean left, List<List<Integer>> result) {
 		if (root == null) {
-			return;
+			return result;
 		}
-		List<Integer> list = new ArrayList<>();
-		if (left) {
-			if (root.left != null) {
-				list.add(root.left.val);
-			}
-			if (root.right != null) {
-				list.add(root.right.val);
-			}
-		} else {
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
 
-			if (root.right != null) {
-				list.add(root.right.val);
+		boolean flag = true;
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			List<Integer> subList = new ArrayList<>();
+
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				subList.add(node.val);
+
+				if (node.left != null) {
+					queue.add(node.left);
+				}
+				if (node.right != null) {
+					queue.add(node.right);
+				}
 			}
-			if (root.left != null) {
-				list.add(root.left.val);
+
+			if (!flag) {
+				Collections.reverse(subList);//翻转
 			}
+
+			result.add(subList);
+			flag = !flag;
 		}
-
-		if (list.isEmpty()) {
-			return;
-		}
-
-		result.add(list);
-
-		traverse(root.left, !left, result);
-		traverse(root.right, !left, result);
+		return result;
 	}
 }
