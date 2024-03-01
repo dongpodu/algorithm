2f0cd23897840319315e26e6512c3be46b30b6a1
willdu <695465951@qq.com>
2024-03-01 09:29:06 +0800

    搜索旋转数组

--- src/me/will/algorithm/array/SearchInRotatedSortArray.java
+++ src/me/will/algorithm/array/SearchInRotatedSortArray.java
@@ -6,46 +6,39 @@ public class SearchInRotatedSortArray {
 			return -1;
 		}
 
-		return search1(nums, target, 0, nums.length - 1);
-
-	}
-
-	public static int search1(int[] nums, int target, int start, int end) {
-		int middle = end - start / 2;
-		if (nums[start] < nums[middle]) {
-			if (nums[start] <= target && target <= nums[middle]) {
-				return binarySearch(nums, target, start, middle);
-			} else {
-				return search1(nums, target, middle, end);
+		int left = 0;
+		int right = nums.length - 1;
+		while (left <= right) {
+			int middle = (left + right) >> 1;
+			if (nums[middle] == target) {
+				return middle;
 			}
-		} else {
-			if (nums[middle] <= target && target <= nums[end]) {
-				return binarySearch(nums, target, middle, end);
-			} else {
-				return search1(nums, target, start, middle);
-			}
-		}
-	}
-
-	public static int binarySearch(int[] nums, int target, int start, int end) {
-		if (end - start <= 2) {
-			for (int i = start; i < end; i++) {
-				if (nums[i] == target) {
-					return i;
+			//左半部分递增
+			if (nums[left] <= nums[middle]) {
+				//target在左半边
+				if (nums[left] <= target && target < nums[middle]) {
+					right = middle - 1;
+				}
+				//target在右半边
+				else {
+					left = middle + 1;
 				}
 			}
-			return -1;
+			//右半部分递增
+			else {
+				//target在右半边
+				if (nums[middle] < target && target <= nums[right]) {
+					left = middle + 1;
+				}
+				//target在左半边
+				else {
+					right = middle - 1;
+				}
+			}
 		}
-
-		int middle = end - start / 2 + start;
-		if (nums[middle] == target) {
-			return middle;
-		} else if (nums[middle] < target) {
-			return binarySearch(nums, target, middle, end);
-		} else {
-			return binarySearch(nums, target, start, middle);
-		}
+		return -1;
 	}
+
 
 	public static void main(String[] args) {
 		int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
