7345ee2a3ecdb7adad0ed505c7207c915915f893
willdu <695465951@qq.com>
2024-03-06 09:04:01 +0800

    最大子序和

--- src/me/will/algorithm/MaxProfit.java
+++ src/me/will/algorithm/MaxProfit.java
@@ -3,7 +3,7 @@ package me.will.algorithm;
 /**
  * 最佳股票买卖时间
  * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/description/
- *
+ * <p>
  * https://zhuanlan.zhihu.com/p/53262788
  */
 public class MaxProfit {
@@ -11,11 +11,8 @@ public class MaxProfit {
 		int profit = 0;
 		int min = Integer.MAX_VALUE;
 		for (int i = 0; i < prices.length; i++) {
-			if (prices[i] < min) {
-				min = prices[i];
-			} else if (profit < prices[i] - min) {
-				profit = prices[i] - min;
-			}
+			min = Math.min(min, prices[i]);
+			profit = Math.max(profit, prices[i] - min);
 		}
 		return profit;
 	}
