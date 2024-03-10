e544937c7d03dd569f4b990ee8a05739cb83b16e
willdu <695465951@qq.com>
2024-03-10 17:01:47 +0800

    1

---
+++ src/me/will/algorithm/array/Merge.java
@@ -0,0 +1,13 @@
+package me.will.algorithm.array;
+
+/**
+ * https://leetcode.cn/problems/merge-intervals/
+ */
+public class Merge {
+
+	public int[][] merge(int[][] intervals) {
+		int[][] result = intervals;
+		return result;
+	}
+
+}
---
+++ src/me/will/algorithm/array/Permute.java
@@ -0,0 +1,14 @@
+package me.will.algorithm.array;
+
+import java.util.List;
+
+/**
+ * 全排列
+ * https://leetcode.cn/problems/permutations/
+ */
+public class Permute {
+
+	public static List<List<Integer>> permute(int[] nums) {
+		return null;
+	}
+}
