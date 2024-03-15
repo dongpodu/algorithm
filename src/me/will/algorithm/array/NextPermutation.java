c3053e075dee4f031ed7420b7781cf67614ed1f6
willdu <695465951@qq.com>
2024-03-15 08:27:07 +0800

    1

--- src/me/will/algorithm/array/NextPermutation.java
+++ src/me/will/algorithm/array/NextPermutation.java
@@ -6,29 +6,36 @@ import java.util.List;
  * https://leetcode.cn/problems/next-permutation/
  */
 public class NextPermutation {
+
+	/**
+	 * https://biaodigit.github.io/LeetCode/0031/#%E9%A2%98%E7%9B%AE%EF%BC%9A%E4%B8%8B%E4%B8%80%E4%B8%AA%E6%8E%92%E5%88%97
+	 *
+	 * @param nums
+	 */
 	public static void nextPermutation(int[] nums) {
-		int index = -1;
-		for (int i = nums.length - 1; i > 0; i--) {
-			//跟前一个数比较，如果大于前一个数，则与前一个数互换位置
-			if (nums[i] > nums[i - 1]) {
-				switchPos(i, i - 1, nums);
-				index = i;
-				break;
-			}
+		//分界点索引,分界点右边是一个倒序递增序列
+		int index = nums.length - 1;
+		while (index > 0 && nums[index] <= nums[index - 1]) {
+			index--;
 		}
 
+		//从分界点右边递增序列中找到比分界点前一位大的数，然后互换位置
 		if (index > 0) {
-			for (int i = index; i < nums.length - 1; i++) {
-				//跟后一个数比较，如果大于后一个数，则与后一个数互换位置
-				if (nums[i] > nums[i + 1]) {
-					switchPos(i, i + 1, nums);
-					break;
-				}
+			int left = index - 1;
+			int right = nums.length - 1;
+			while (right >= 0 && nums[left] >= nums[right]) {
+				right--;
 			}
-		} else {
-			for (int left = 0, right = nums.length - 1; left < right; left++, right--) {
-				switchPos(left, right, nums);
-			}
+			switchPos(left, right, nums);
+		}
+
+		//再将右边序列调整成最小序列
+		int left = index;
+		int right = nums.length - 1;
+		while (left < right) {
+			switchPos(left, right, nums);
+			left++;
+			right--;
 		}
 	}
 
@@ -39,7 +46,7 @@ public class NextPermutation {
 	}
 
 	public static void main(String[] args) {
-		int[] nums = new int[]{3, 2, 1};
+		int[] nums = new int[]{1, 5, 1};
 		nextPermutation(nums);
 		System.out.println(List.of(nums));
 	}
