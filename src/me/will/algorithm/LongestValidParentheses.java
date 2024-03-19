e3a55d0c1770bb681290fbce3e16e86af4df646f
willdu <695465951@qq.com>
2024-03-19 09:40:11 +0800

    1

---
+++ src/me/will/algorithm/LongestValidParentheses.java
@@ -0,0 +1,41 @@
+package me.will.algorithm;
+
+import java.util.Stack;
+
+public class LongestValidParentheses {
+
+	/**
+	 * https://leetcode-solution-leetcode-pp.gitbook.io/leetcode-solution/hard/32.longest-valid-parentheses
+	 *
+	 * @param s
+	 * @return
+	 */
+	public static int longestValidParentheses(String s) {
+		//栈存的是下标
+		Stack<Integer> stack = new Stack<>();
+		stack.push(-1);
+		int longest = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == '(') {
+				stack.push(i);
+			} else {
+				if (!stack.isEmpty()) {
+					stack.pop();
+				}
+				if (stack.isEmpty()) {
+					stack.push(i);
+				} else {
+					longest = Math.max(longest, i - stack.elementAt(stack.size() - 1));
+				}
+			}
+		}
+		return longest;
+	}
+
+	public static void main(String[] args) {
+		String s = ")()())";
+		System.out.println(longestValidParentheses(s));
+	}
+
+}
